0ddbf42131eb9dfc7b468dc15f5985444a909028
Lekkala Pavani <pavani.leela15@gmail.com>
2024-09-19 13:15:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+import AddStudent from './components/AddStudent/index';
+import StudentDetails from './components/StudentDetails/index'
+import Navbar from './components/Navbar';
+
+// const API_URL = "http://localhost:3001/students";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<AddStudent/>}/>
+      <Route path="/studentdetails" element={<StudentDetails/>}/>
+
+    </Routes>
+    </BrowserRouter>
+   
+  )
 }
+
+
+
+
 
 export default App;
